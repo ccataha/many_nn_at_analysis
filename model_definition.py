7fa72c946cc762505fc3316d7bce4d94e8e29bf9
Ludwing <ludwing238@gmail.com>
2020-10-16 22:21:38 -0600

    create model definition

--- model_definition.py
+++ model_definition.py
@@ -7,16 +7,17 @@ def defineModel():
     print("\n\nTensorflow Model:")
     # Define new Model for rnn
     model = tf.keras.models.Sequential()
-    # Adding model type
-    model.add(tf.keras.layers.SimpleRNN(4))
+    # Adding model type with 41 features
+    model.add(tf.keras.layers.SimpleRNN(41))
     # Adding Hidden Layers
-    model.add(tf.keras.layers.Dense(units=3,activation='sigmoid'))
-    model.add(tf.keras.layers.Dense(units=5,activation='sigmoid'))
-    model.add(tf.keras.layers.Dense(units=8,activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(units=80,activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(units=160,activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(units=240,activation='sigmoid'))
     # Adding output layer
-    model.add(tf.keras.layers.Dense(units=2,activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(units=2,activation='softmax'))
 
     model.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=0.001),
                 loss=tf.keras.losses.mean_squared_error,
                 metrics=['accuracy'])
+
     return model
