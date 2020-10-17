48d1689280118a919f0fc363f9d83899627bbd94
Ludwing <ludwing238@gmail.com>
2020-10-16 22:19:16 -0600

    update  model operation class

--- model_operation.py
+++ model_operation.py
@@ -1,36 +1,47 @@
 # Class to transformate data
 import controller.transformation as trans
 import controller.normalization as norm
+import tensorflow as tf
+from tensorflow import keras
+import model_results as mrs
 import numpy as np
 
 def transformData(data):
-    print("Transform and Normalize Data")
     # Transform text to number
     print("Transform text to number")
     data = trans.transformData(data)
+    data["label"]
     # Split and remove columns
     print("Remove unused columns")
     kddCupY = data["label"]
+    kddCupY = kddCupY.to_numpy()    
+    kddCupY = keras.utils.to_categorical(kddCupY,2)
     kddCupX = data
     kddCupX.pop("label")
-    kddCupX.pop("service")
+    kddCupX = kddCupX.to_numpy()
     # Normalize data
     print("Normalize Colum Data")
     normalizeTrainingDataX = norm.normalizeColumn(kddCupX)
-    arrayTrainingDataY = np.array(kddCupY)
     # Reshape
     print("Reshaping Data")
     normalizeTrainingDataX = normalizeTrainingDataX.reshape((normalizeTrainingDataX.shape[0],normalizeTrainingDataX.shape[1],-1))
-    return normalizeTrainingDataX, arrayTrainingDataY
+    return normalizeTrainingDataX, kddCupY
 
-def trainModel(epochs,  model, trainingData):
+def trainModel(epochs,  model, trainingData, validateData):
+    print("Transform and Normalize Training")
     normalizeTrainingDataX, arrayTrainingDataY = transformData(trainingData)
+    print("Transform and Normalize Validation")
+    validateDataX, validateDataY = transformData(validateData)
+
     # Train Model
     print("\n\nTraining model with new data")
-    model.fit(normalizeTrainingDataX,arrayTrainingDataY,epochs=epochs,batch_size=25,shuffle=True)
+    model.fit(normalizeTrainingDataX,arrayTrainingDataY,epochs=epochs,batch_size=25,shuffle=True,
+    validation_data=(validateDataX,validateDataY))
+    
     results = model.predict(normalizeTrainingDataX)
+
     print("\n\nResults:")
-    showSummary(results,arrayTrainingDataY)
+    mrs.showSummary(arrayTrainingDataY,results)
     return model
 
 def validateModel(model, validateData):
@@ -38,62 +49,5 @@ def validateModel(model, validateData):
     print("\n\nValidate Model with new data")
     results = model.predict(normalizeValidateDataX)
     print("\n\nResults:")
-    showSummary(results,arrayValidateDataY)
+    mrs.showSummary(results,arrayValidateDataY)
     return model
-
-def confirmValidate():
-    confirm = input("\n\nDo you want to [v]validate model or [e]exit: ?")
-    if confirm != 'v' and confirm != 'e':
-        print("\n\nInvalid Option. Please Enter a Valid Option.")
-        confirmValidate() 
-    print (confirm)
-    return confirm
-
-
-def showSummary(result,validationData):
-    printStats(getStats(validationData,result))
-
-def getStats(real,predicted):
-    numClasses = real.shape[1]
-    stats = []
-    for i in range(0,numClasses):
-        stats.append([])
-        for j in range(0,numClasses):
-            stats[i].append(0)
-    for i in range(0,len(predicted)):
-        p = np.argmax(predicted[i])
-        r = np.argmax(real[i])
-        stats[p][r]+=1
-    metrics = {}
-    for i in range(0,numClasses):
-        tp = stats[i][i]
-        fp = 0
-        fn = 0
-        for j in range(0,numClasses):
-            if (i!=j):
-                fp += stats[i][j]
-                fn += stats[j][i]
-        if (tp+fp) > 0: precision = tp/(tp+fp)
-        else: precision = -1
-        if (tp+fn) > 0: recall = tp/(tp+fn)
-        else: recall = -1
-        if precision >= 0 and recall >= 0 and (precision+recall>0): f1 = 2*precision*recall/(precision+recall)        
-        else: f1 = -1
-        metrics[i]= {'precision': precision,
-                     'recall': recall,
-                     'f1': f1 }
-    return stats,metrics
-
-def printStats(stats):
-    cm,m=stats
-    print("\nConfusion Matrix")
-    print("P \ R\tnormal\tanomaly")
-    print("normal\t"+str(cm[0][0])+"\t"+str(cm[0][1]))
-    print("anomaly\t"+str(cm[1][0])+"\t"+str(cm[1][1]))
-    print("")
-    print("Metrics")
-    print("Conexion\tPrecision\tRecall\tF-1")
-    print("normal\t"+str(m[0]['precision'])+"\t"+str(m[0]['recall'])+"\t"+str(m[0]["f1"]))
-    print("anomaly\t"+str(m[1]['precision'])+"\t"+str(m[1]['recall'])+"\t"+str(m[1]["f1"]))
-    print("\n")
-  
